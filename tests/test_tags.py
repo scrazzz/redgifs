e18543ad830bcb563460763b7cc8f8f1f152ef98
scrazzz <scruzism@mail.com>
2022-08-26 13:23:05 +0300

    Add test for single_random method

---
+++ tests/test_tags.py
@@ -0,0 +1,25 @@
+import pytest
+
+from redgifs import Tags
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ('tits', 'Tits'),
+        ('ass', 'Ass'),
+        ('cum', 'Cum'),
+        ('americam', 'American'),    # on purpose
+        ('japanes', 'Japanese'),     # on purpose
+        ('hitomi tanaka', 'Hitomi Tanaka'),
+        ('big dick', 'Big Dick'),
+        ('hige tits', 'Huge Tits'),  # on purpose
+        ('alexa pearl', 'Alexa Pearl'),
+        ('ava adams', 'Ava Addams'), # on purpose
+    ]
+)
+def test_tags_search(test_input, expected):
+    searched = Tags.search(test_input)
+    assert searched == expected
+
+def test_tags_single_random():
+    assert bool(Tags.single_random()) == True
