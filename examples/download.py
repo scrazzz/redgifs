ffd67cd2b47bd6aa4dc8ee3202e9f49290241b25
scrazzz <scruzism@mail.com>
2022-10-08 10:57:51 +0300

    Add download example

---
+++ examples/download.py
@@ -0,0 +1,33 @@
+"""
+This example shows how you can download GIFs from RedGIFs using this library.
+"""
+
+from redgifs import API
+
+api = API()
+
+# Here we are searching for a GIF and only returning 15 of it.
+count = 10
+result = api.search_gif('hitomi tanaka', count=count)
+
+# Check if your searched GIF exists.
+# If not, print an error message and halt the program.
+if result.gifs is None:
+    print(f'No results found for {result.searched_for}')
+    exit(1)
+
+
+# Here we are downloading the gifs in the current directory.
+# If you want to move it to a specific directory, then make sure you have
+# created that folder first and use "/dir_name/video{n}.mp4" where dir_name is
+# the folder that you have created.
+n = 1
+for gif in result.gifs:
+    api.download(gif.urls.hd, f'video{n}.mp4')
+    # It's good to see where we are at while this is downloading.
+    print(f'Downloading {n}/{count}')
+    # Increment the number after a download is complete to keep
+    # track of the total downloaded GIFs.
+    n += 1
+
+print('Download complete')
--- examples/tag.py
+++ examples/tag.py
@@ -6,6 +6,21 @@ for searching for gifs. Not the best way to search.
 from redgifs import API, Tags
 
 api = API()
-# Helpful to find a tag if you're using an IDE with autocomplete
+
+# Helpful to find a tag if you're using an IDE with autocomplete.
 result = api.search(Tags.brunette)
-print(result.total)
+
+# If you want to search manually, use Tags.search which returns a
+# list of closest tag names associated to it.
+tags = Tags.search('autumn falls')
+result = api.search(tags[0])  # Get the 1st one from the list of closest tag names
+
+# If you want to get a random tag.
+# This will give you 4 random Tags.
+tags = Tags.random(count=4)
+result = api.search(tags[2])  # Get the 3rd random tag
+
+# Sometimes you just want a single random tag.
+# You could to the above method and use "tags[0]" to get it or alternatively:
+tag = Tags.single_random()
+result = api.search(tag)
