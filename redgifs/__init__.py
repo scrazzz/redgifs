8eaf58f5f4636da113ec35af396496ea2b9ed3d4
scrazzz <scruzism@mail.com>
2023-08-15 13:21:53 +0300

    Bump version v1.7.1a

--- redgifs/__init__.py
+++ redgifs/__init__.py
@@ -26,7 +26,7 @@ __title__ = 'redgifs'
 __author__ = 'scrazzz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2022-present scrazzz'
-__version__ = '1.7.0'
+__version__ = '1.7.1a'
 
 from typing import Literal, NamedTuple
 
@@ -42,6 +42,6 @@ class VersionInfo(NamedTuple):
     micro: int
     releaselevel: Literal['alpha', 'beta', 'final']
 
-version_info: VersionInfo = VersionInfo(major=1, minor=7, micro=0, releaselevel='final')
+version_info: VersionInfo = VersionInfo(major=1, minor=7, micro=1, releaselevel='alpha')
 
 del NamedTuple, VersionInfo
