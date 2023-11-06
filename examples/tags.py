cf3d4b0f202a9057a3fd3e1def664a8923a12159
scrazzz <scruzism@mail.com>
2023-11-06 19:25:03 +0530

    Add example for using Tags

---
+++ examples/tags.py
@@ -0,0 +1,28 @@
+"""
+This example shows how to use tags for searching.
+
+Only use this if you really need to, otherwise just search for stuff 
+the normaly way (take a look at the main example in the README).
+"""
+
+from redgifs import API, Tags
+
+def main():
+    # Login to the RedGifs API
+    api = API().login()
+
+    # Create an instance of Tags
+    tags = Tags()
+
+    # Here we input the search term. The result is a list of tags with the closest tag name.
+    # You could use this if you are taking in a user input or looking up specific pornstar models but don't
+    # know their proper spelling, etc..
+    # This will error if it can't find a proper tag.
+    query = tags.search('search term here')[0]
+
+    # Using the query:
+    result = api.search_gif(query)
+    print(result.gifs)
+
+if __name__ == '__main__':
+    main()
