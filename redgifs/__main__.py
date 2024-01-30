834ce32cbe5e604ac5d9363994033708019a44ec
test-account-svg <66283876+test-account-svg@users.noreply.github.com>
2024-01-29 22:05:38 -0800

    Fix multi-page user scrape.
    When scraping a user's profile with multiple pages, the last page is currently skipped. This should fix that.

--- redgifs/__main__.py
+++ redgifs/__main__.py
@@ -132,7 +132,7 @@ def start_dl(url: str, *, folder: Optional[str], quality: str) -> None:
             print(f'\nDownloaded {done}/{total} videos of "{user}" {f"to {folder} folder" if folder else ""} sucessfully')
 
         # If there's more than 1 page
-        while curr_page != total_pages:
+        while curr_page <= total_pages:
             for gif in total_gifs:
                 gif = get_quality(quality, gif)
                 filename = f'{gif.split("/")[3].split(".")[0]}.mp4'
