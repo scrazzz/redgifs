85165c85646fd79e99cb60f5c4870c654c2f6c35
scrazzz <scruzism@mail.com>
2024-02-06 12:43:50 +0530

    [no ci] Suppress sphinx autosectionlabel warnings

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,8 @@ extensions = [
     'sphinx.ext.autosectionlabel',
 ]
 
+suppress_warnings = ['autosectionlabel.*']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
