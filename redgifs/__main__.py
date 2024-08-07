5985b9bd71fe5fc9d597cb02178cd3a2fd96b93e
scrazzz <scruzism@mail.com>
2024-08-07 15:51:25 +0530

    Add new CLI interface

--- redgifs/__main__.py
+++ redgifs/__main__.py
@@ -22,166 +22,120 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-# TODO: Use rich for pretty console messages.
+from __future__ import annotations
 
+import click
 import re
-import sys
-import argparse
-import platform
-import importlib.metadata
-from typing import Optional
-
-import aiohttp
-import requests
-from yarl import URL
+import yarl
+from typing import Iterable, Optional, TYPE_CHECKING
 
 import redgifs
-from redgifs.models import GIF
 
-parser = argparse.ArgumentParser(prog='redgifs')
-parser.add_argument('link', nargs='?', help='Enter a RedGifs URL', metavar='URL')
-parser.add_argument('--folder', help='Folder to download the video(s) to.', metavar='FOLDER')
-parser.add_argument('--list', help='Download GIFs from a txt file containing URLs seperated by a newline.', metavar='FILE')
-parser.add_argument('--version', help='Show redgifs version info.', action='store_true')
-parser.add_argument('--quality', help='The video quality of the GIF to download. Available options are: "sd" and "hd".', metavar='QUALITY', default="hd")
-args = parser.parse_args()
+if TYPE_CHECKING:
+    from redgifs.models import GIF
 
-session = requests.Session()
-client = redgifs.API(session=session)
+client = redgifs.API().login()
 
-USERNAME_RE = re.compile(r'https:\/\/(www\.)?redgifs\.com\/users\/(?P<username>\w+)')
-
-def show_version() -> None:
-    entries = []
-
-    entries.append('- Python v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}'.format(sys.version_info))
-    version_info = redgifs.version_info
-    entries.append('- redgifs v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}'.format(version_info))
-    if version_info.releaselevel != 'final':
-        version = importlib.metadata.version('redgifs')
-        if version:
-            entries.append(f'    - redgifs metadata: v{version}')
-
-    entries.append(f'- aiohttp v{aiohttp.__version__}')
-    uname = platform.uname()
-    entries.append('- system info: {0.system} {0.release} {0.version}'.format(uname))
-    print('\n'.join(entries))
-
-def get_quality(q: str, gif: GIF) -> str:
-    if q.lower() not in ['sd', 'hd']:
-        raise TypeError('Valid quality options are "sd" or "hd"')
-    return gif.urls.sd if q.lower() == 'sd' else gif.urls.hd
-
-def start_dl(url: str, *, folder: Optional[str], quality: str) -> None:
-    yarl_url = URL(url)
-    if 'redgifs' not in str(yarl_url.host):
-        raise TypeError(f'"{url}" is not a valid redgifs URL')
-
-    # Handle 'normal' URLs, i.e, a direct link from browser (eg: "https://redgifs.com/watch/deeznuts")
-    if 'watch' in yarl_url.path:
-        id = yarl_url.path.split('/')[-1]
-        gif = client.get_gif(id)
-        gif_url = get_quality(quality, gif)
-        filename = f'{gif_url.split("/")[3].split(".")[0]}.mp4'
-        print(f'Downloading {id}...')
-        if folder:
-            client.download(gif_url, f'{folder}/{filename}')
-        else:
-            client.download(gif_url, f'{filename}')
-        print('Download complete.\n')
-        return
-
-    # Handle /users/ URLs (eg: https://redgifs.com/users/redgifs)
-    if '/users/' in yarl_url.path:
-        match = re.match(USERNAME_RE, str(yarl_url))
-        if not match:
-            raise TypeError(f'Not a valid /users/ URL: {yarl_url}')
-        user = match.groupdict()['username']
-        data = client.search_creator(user)
-        curr_page = data.page
-        total_pages = data.pages
-        total_gifs = data.gifs
-        total = data.total
-        done = 0
-
-        # Case where there is only 1 page
-        if curr_page == total_pages:
-            for gif in total_gifs:
-                gif = get_quality(quality, gif)
-                filename = f'{gif.split("/")[3].split(".")[0]}.mp4'
-                try:
-                    if folder:
-                        client.download(gif, f'{folder}/{filename}')
-                    else:
-                        client.download(gif, f'{filename}')
-                    done += 1
-                    print(f'Downloaded {done}/{total} GIFs')
-                except Exception as e:
-                    if isinstance(e, FileNotFoundError):
-                        print(f'[!] An error occured while downloading: {e}\nMake sure you have a folder called "{folder}" in the current directory.')
-                        exit(1)
-                    else:
-                        print(f'[!] Error occurred when downloading {url}:\n{e}. Continuing...')
-                        continue
-
-            print(f'\nDownloaded {done}/{total} videos of "{user}" {f"to {folder} folder" if folder else ""} sucessfully')
-            return
-
-        # If there's more than 1 page
-        while curr_page <= total_pages:
-            for gif in total_gifs:
-                gif = get_quality(quality, gif)
-                filename = f'{gif.split("/")[3].split(".")[0]}.mp4'
-                try:
-                    if folder:
-                        client.download(gif, f'{folder}/{filename}')
-                    else:
-                        client.download(gif, filename)
-                    done += 1
-                    print(f'Downloaded {done}/{total} GIFs')
-                except Exception as e:
-                    if isinstance(e, FileNotFoundError):
-                        print(f'[!] An error occured while downloading: {e}\nMake sure you have a folder called "{folder}" in the current working directory.')
-                        exit(1)
-                    else:
-                        print(f'[!] Error occurred when downloading {url}:\n{e}. Continuing...')
-                        continue
-
-            # If we are in the last page, break the loop
-            if curr_page == total_pages:
-                break
-
-            curr_page += 1
-            total_gifs.clear()
-            data = client.search_creator(user, page=curr_page)
-            total_gifs.extend(data.gifs)
-
-        print(f'\nDownloaded {done}/{total} videos of "{user}" {f"to {folder} folder" if folder else ""} sucessfully')
-        return
-
+def download_gif(url: yarl.URL, quality: str, folder: Optional[str]):
+    id = url.path.split('/')[-1]
+    gif = client.get_gif(id)
+    gif_url = gif.urls.sd if quality == 'sd' else gif.urls.hd
+    filename = f'{gif_url.split("/")[3].split(".")[0]}.mp4'
+    click.echo(f'Downloading {id}...')
+    if folder:
+        client.download(gif_url, f'{folder}/{filename}')
     else:
-        print('[!] Invalid URL')
+        client.download(gif_url, f'{filename}')
+        click.echo('Download complete.')
+
+def _dl_with_args(gif: GIF, quality: str, folder: Optional[str]):
+    gif_url = gif.urls.sd if quality == 'sd' else gif.urls.hd
+    filename = f'{gif_url.split("/")[3].split(".")[0]}.mp4'
+    if folder:
+        client.download(gif_url, f'{folder}/{filename}')
+    else:
+        client.download(gif_url, f'{filename}')
+
+def download_users_gifs(url: yarl.URL, quality: str, folder: Optional[str]):
+    match = re.match(r'https://(www\.)?redgifs\.com\/users\/(?P<username>\w+)', str(url))
+    if not match:
+        click.UsageError(f'Not a valid redgifs user URL: {url}')
         exit(1)
 
-def main() -> None:
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
+    user = match.groupdict()['username']
+    data = client.search_creator(user)
+    curr_page = data.page
+    total_pages = data.pages
+    total_gifs_in_page = data.gifs
+    total = data.total
+    done = 0
 
-    if args.link:
-        client.login()
-        start_dl(args.link, folder=args.folder, quality=args.quality)
-        exit(0)
+     # Case where there is only 1 page
+    if curr_page == total_pages:
+        for gif in total_gifs_in_page:
+            try:
+                _dl_with_args(gif, quality, folder)
+                done += 1
+                click.echo(f'Downloaded {done}/{total} GIFs')
+            except Exception as e:
+                if isinstance(e, FileNotFoundError):
+                    click.UsageError(f'[!] An error occured while downloading: {e}\nMake sure you have a folder called "{folder}" in the current directory.')
+                    exit(1)
+                else:
+                    click.UsageError(f'[!] Error occurred when downloading {url}:\n{e}. Continuing...')
+                    continue
+        click.echo(f'\nDownloaded {done}/{total} videos of "{user}" {f"to {folder} folder" if folder else ""} sucessfully')
 
-    if args.list:
-        client.login()
-        with open(args.list) as f:
-            for url in f.readlines():
-                start_dl(url, folder=args.folder, quality=args.quality)
-            exit(0)
+    # Case where there is more than 1 page
+    while curr_page <= total_pages:
+        for gif in total_gifs_in_page:
+            try:
+                _dl_with_args(gif, quality, folder)
+                done += 1
+                click.echo(f'Downloaded {done}/{total} GIFs')
+            except Exception as e:
+                if isinstance(e, FileNotFoundError):
+                    click.UsageError(f'[!] An error occured while downloading: {e}\nMake sure you have a folder called "{folder}" in the current working directory.')
+                    exit(1)
+                else:
+                    click.echo(f'[!] Error occurred when downloading {url}:\n{e}. Continuing...')
+                    continue
 
-    if args.version:
-        show_version()
+        # If we are in the last page, break the loop
+        if curr_page == total_pages:
+            break
 
-if __name__ == '__main__':
-    main()
+        curr_page += 1
+        total_gifs_in_page.clear()
+        data = client.search_creator(user, page=curr_page)
+        total_gifs_in_page.extend(data.gifs)
+
+    click.echo(f'\nDownloaded {done}/{total} videos of "{user}" {f"to {folder} folder" if folder else ""} sucessfully')
+
+@click.command()
+@click.argument('urls', nargs=-1)
+@click.option('-q', '--quality', type=click.Choice(['sd', 'hd']), default='hd', show_default=True, help='Video quality of GIF to download.')
+@click.option('--use-dir', 'folder', help='The folder/directory to save the downloads to.', metavar='FOLDER_NAME')
+@click.option('--file', help='Download URLs from a newline seperated txt file.', type=click.Path(exists=True))
+@click.pass_context
+def cli(ctx: click.Context, urls: Iterable[str], quality: str, folder: Optional[str], file: Optional[str]) -> None:
+    if not urls and not file:
+        click.echo(ctx.command.get_help(ctx))
+
+    if file:
+        with open(file) as f:
+            [download_gif(url, quality, folder) for url in [yarl.URL(line) for line in f.readlines()]]
+
+    for url in urls:
+        url = yarl.URL(url)
+        if 'redgifs' not in str(url.host):
+            click.UsageError(f'"{url}" is not a valid redgifs URL').show()
+            exit(1)
+
+        # Handle 'normal' URLs, i.e, a direct link from browser (eg: "https://redgifs.com/watch/deeznuts")
+        if '/watch/' in url.path:
+            download_gif(url, quality, folder)
+
+        # Handle /users/ URLs (eg: https://redgifs.com/users/redgifs)
+        if '/users/' in url.path:
+            download_users_gifs(url, quality, folder)
