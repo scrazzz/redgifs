331cad3ed82e448a34b9b0b58bd15903d044a000
theintrepidfox <theintrepidfox@live.com>
2024-11-14 18:22:32 -0800

    Allow downloading JPEGs

--- redgifs/http.py
+++ redgifs/http.py
@@ -233,7 +233,7 @@ class HTTP:
             _log.debug(f'GET {url} returned code: {r.status_code}')
 
             content_type = r.headers['Content-Type']
-            if content_type != 'video/mp4':
+            if content_type not in ('video/mp4', 'image/jpeg'):
                 _log.error(f'GET {url} returned improper content-type: {content_type}')
                 raise TypeError(f'"{url}" returned invalid content type for downloading: {content_type}')
 
