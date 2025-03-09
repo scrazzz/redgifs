930d2ce72ed65e2ab81a7f5fcd6c3bb73d802919
scrazzz <scruzism@mail.com>
2025-03-09 12:41:28 +0530

    Refactor download functions to use Path for folder parameters and improve download status messages
    Breaking change: --dir to --folder

--- redgifs/__main__.py
+++ redgifs/__main__.py
@@ -27,6 +27,8 @@ from __future__ import annotations
 import re
 import sys
 import platform
+import itertools
+from pathlib import Path
 from typing import Iterable, Optional, TYPE_CHECKING
 
 import click
@@ -39,7 +41,7 @@ from . import __version__
 if TYPE_CHECKING:
     from redgifs.models import GIF, Image
 
-def download_gif(client, url: yarl.URL, quality: str, folder: Optional[str], *, skip_check: bool = False):
+def download_gif(client, url: yarl.URL, quality: str, folder: Optional[Path], *, skip_check: bool = False):
     # If skip_check is true then this will be the GIF's ID and splitting the URL is not required
     id = str(url).lower() if skip_check else url.path.split('/')[-1]
     gif = client.get_gif(id)
@@ -50,7 +52,7 @@ def download_gif(client, url: yarl.URL, quality: str, folder: Optional[str], *, 
     click.echo(f'Downloading {id}...')
     click.echo('Download complete.')
 
-def _dl_with_args(client, gif: GIF | Image, quality: str, folder: Optional[str], is_image: bool):
+def _dl_with_args(client, gif: GIF | Image, quality: str, folder: Optional[Path], is_image: bool):
     gif_url = gif.urls.sd if quality == 'sd' else gif.urls.hd
     filename = f'{gif_url.split("/")[3].split(".")[0]}.mp4'
     if is_image:
@@ -62,7 +64,7 @@ def _dl_with_args(client, gif: GIF | Image, quality: str, folder: Optional[str],
     else:
         client.download(gif_url, f'{filename}')
 
-def download_users_gifs(client, url: yarl.URL, quality: str, folder: Optional[str], images_only: bool):
+def download_users_gifs(client, url: yarl.URL, quality: str, folder: Optional[Path], images_only: bool):
     match = re.match(r'https://(www\.)?redgifs\.com\/users\/(?P<username>\w+)', str(url))
     if not match:
         click.UsageError(f'Not a valid redgifs user URL: {url}')
@@ -80,34 +82,30 @@ def download_users_gifs(client, url: yarl.URL, quality: str, folder: Optional[st
 
      # Case where there is only 1 page
     if curr_page == total_pages:
+        spinner = itertools.cycle(["-", "\\", "|", "/"])
         for gif in total_gifs_in_page:
             try:
                 _dl_with_args(client, gif, quality, folder, is_image)
                 done += 1
-                click.echo(f'Downloaded {done}/{total} GIFs')
+                click.echo(f'\r{next(spinner)} Downloaded {done}/{total} {"GIFs" if not is_image else "image"}', nl=False)
             except Exception as e:
-                if isinstance(e, FileNotFoundError):
-                    click.UsageError(f'[!] An error occured while downloading: {e}\nMake sure you have a folder called "{folder}" in the current directory.')
-                    exit(1)
-                else:
-                    click.UsageError(f'[!] Error occurred when downloading {url}:\n{e}. Continuing...')
-                    continue
-        click.echo(f'\nDownloaded {done}/{total} videos of "{user}" {f"to {folder} folder" if folder else ""} sucessfully')
+                click.UsageError(f'[!] An error occurred when downloading {url}:\n{e}. Continuing...')
+                continue
+
+        click.echo(f'\r[-] Downloaded {done}/{total} {"videos" if not is_image else "images"} of user {user} {f"to folder '{folder}'" if folder else ""} sucessfully!')
+        exit(0)
 
     # Case where there is more than 1 page
     while curr_page <= total_pages:
+        spinner = itertools.cycle(["-", "\\", "|", "/"])
         for gif in total_gifs_in_page:
             try:
                 _dl_with_args(client, gif, quality, folder, is_image)
                 done += 1
-                click.echo(f'Downloaded {done}/{total} GIFs')
+                click.echo(f'\r{next(spinner)} Downloaded {done}/{total} {"GIFs" if not is_image else "image"}', nl=False)
             except Exception as e:
-                if isinstance(e, FileNotFoundError):
-                    click.UsageError(f'[!] An error occured while downloading: {e}\nMake sure you have a folder called "{folder}" in the current working directory.')
-                    exit(1)
-                else:
-                    click.echo(f'[!] Error occurred when downloading {url}:\n{e}. Continuing...')
-                    continue
+                click.echo(f'[!] An error occurred when downloading {url}:\n{e}. Continuing...')
+                continue
 
         # If we are in the last page, break the loop
         if curr_page == total_pages:
@@ -118,17 +116,28 @@ def download_users_gifs(client, url: yarl.URL, quality: str, folder: Optional[st
         data = client.search_creator(user, page=curr_page)
         total_gifs_in_page.extend(data.gifs)
 
-    click.echo(f'\nDownloaded {done}/{total} videos of "{user}" {f"to {folder} folder" if folder else ""} sucessfully')
+    click.echo()
+    click.echo(f'\r[-] Downloaded {done}/{total} {"videos" if not is_image else "images"} of user {user} {f"to folder '{folder}'" if folder else ""} sucessfully!')
 
 @click.command()
 @click.argument('urls', nargs=-1)
 @click.option('-q', '--quality', type=click.Choice(['sd', 'hd']), default='hd', show_default=True, help='Video quality of GIF to download.')
-@click.option('-d', '--dir', 'folder', help='The folder/directory to save the downloads to.', metavar='FOLDER_NAME')
-@click.option('-i', '--input', 'file', help='Download URLs from a newline seperated txt file.', type=click.Path(exists=True))
-@click.option('--images', is_flag=True, help='Download only images from a user profile')
+@click.option(
+    '-f', '--folder',
+    help='The folder to save the downloads to.',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path),
+    metavar='FOLDER_NAME'
+)
+@click.option(
+    '-i', '--input', 'file',
+    help='Download URLs from a newline seperated txt file.',
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    metavar='FILE_NAME'
+)
+@click.option('--images', is_flag=True, help='Download only images from a user profile.')
 @click.option('-v', '--version', is_flag=True, help='Shows currently installed version.')
 @click.pass_context
-def cli(ctx: click.Context, urls: Iterable[str], quality: str, folder: Optional[str], file: Optional[str], version: bool, images: bool) -> None:
+def cli(ctx: click.Context, urls: Iterable[str], quality: str, folder: Optional[Path], file: Optional[str], version: bool, images: bool) -> None:
     if version:
         info = []
         info.append('- python: v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}'.format(sys.version_info))
